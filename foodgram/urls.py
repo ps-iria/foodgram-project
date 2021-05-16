e53b097821e8223772384899ed580cf69b2d429c
илья михайлов <ps.iria@yandex.ru>
2021-05-16 22:54:10 +0700

    Fix menu, add 404,500 error

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -12,6 +12,9 @@ urlpatterns = [
     path('', include('recipes.urls')),
 ]
 
+handler404 = 'foodgram.views.page_not_found'
+handler500 = 'foodgram.views.server_error'
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
