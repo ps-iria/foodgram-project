da04345106245f31e5b2abf4de95c3e16127075e
илья михайлов <ps.iria@yandex.ru>
2021-05-22 11:39:30 +0700

    rev4 stage2

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,9 @@
-from io import BytesIO
-
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfbase import pdfmetrics, ttfonts
-from reportlab.pdfgen import canvas
 
 from recipes import services
 from recipes.forms import CreateRecipeForm
