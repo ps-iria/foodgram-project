a2a60a975254d1d4508fa2f7a9bc3751b16c9e6c
илья михайлов <ps.iria@yandex.ru>
2021-05-22 22:24:09 +0700

    after rev

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '21xc0vp*wr%@go@v!_c!*sbf51+yp*+%e8eey=q$_u3n1rvl#)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
